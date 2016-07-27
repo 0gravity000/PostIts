78e1480461efdc99cde7cffd53ff2c1d93f9c4d3
0gravity000 <0gravity000@gmail.com>
2016-07-27 23:14:45 +0900

    001
    新規作成

---
+++ Pods/Target Support Files/Pods-PostItsUITests/Pods-PostItsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PostItsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PostItsUITests
+@end
---
+++ PostIts/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PostIts
+//
+//  Created by SASAKIAI on 2016/07/27.
+//  Copyright © 2016年 SASAKIAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
