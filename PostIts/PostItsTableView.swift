96c8ac2fb3f78a45792e9d76bcbfda7a0e0c6737
0gravity000 <0gravity000@gmail.com>
2016-07-28 22:01:55 +0900

    006
    PostIts シーンを編集
    PostItsTableView 追加
    PostItsTableViewCell 追加

---
+++ PostIts/PostItsTableView.swift
@@ -0,0 +1,21 @@
+//
+//  PostItsTableView.swift
+//  PostIts
+//
+//  Created by SASAKIAI on 2016/07/28.
+//  Copyright © 2016年 SASAKIAI. All rights reserved.
+//
+
+import UIKit
+
+class PostItsTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
